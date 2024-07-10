2ac70ced97c0d9f6efc3515f6acb17eea959ea56
SCloud7 <69323066+SCloud7@users.noreply.github.com>
2024-07-11 01:40:35 +0200

    Create .h

---
+++ .h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssoukoun <ssoukoun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 16:09:17 by ssoukoun          #+#    #+#             */
+/*   Updated: 2024/07/11 01:24:55 by ssoukoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# include "get_next_line.h"
+# include <fcntl.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*strch(char *str, char c);
+char	*set(char *line);
+char	*lect(char *stock, int fd);
+char	*get_next_line(int fd);
+int		ft_strlen(const char *str);
+
+#endif
